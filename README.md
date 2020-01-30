285440c1fe6cc2192d57ee5b3dd8eb1429368122
crwgregory <crwgregory@gmail.com>
2020-01-30 11:11:38 -0700

    updated readme and handle jwts not buffered already

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # JWT Parser
+## Usage
+`go get github.com/crwgregory/jwt-parser`
+
+```
+jwt-parser eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWUsImp0aSI6IjE2NTg2NWVhLTM5MzEtNGRlYi1hMWRmLWI0ODRhMjYxMDllNSIsImlhdCI6MTU4MDQwNjIwOSwiZXhwIjoxNTgwNDA5ODA5fQ.xbLq9kKUqH1zLGfQ5luqlyMbjo9gJsrOEdcerqIr6so
+
+[
+    {
+        "alg": "HS256",
+        "typ": "JWT"
+    },
+    {
+        "admin": true,
+        "exp": 1580409809,
+        "iat": 1580406209,
+        "jti": "165865ea-3931-4deb-a1df-b484a26109e5",
+        "name": "John Doe",
+        "sub": "1234567890"
+    }
+]
+```
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"log"
 	"fmt"
 	"encoding/json"
 	"strings"
@@ -13,10 +14,21 @@ func main() {
 	jwt := strings.Split(a, ".")
 	parts := []map[string]interface{}{}
 	for i := 0; i < 2; i++ {
-		b, err := base64.StdEncoding.DecodeString(jwt[i])
-		if err != nil {
-			panic(err)
+
+		// make sure the string is divisable by 4
+		part := jwt[i]
+		m := len(part) % 4
+		if m != 0 {
+			for i := 0; i < 4 - m; i++ {
+				part += `=`
+			}
 		}
+
+		b, err := base64.StdEncoding.DecodeString(part)
+		if err != nil {
+			log.Fatalf("base(32|64) decode error: part[%d] %s \n64:%s", i, part, err)				
+		}
+		
 		d := make(map[string]interface{})
 		if err := json.Unmarshal(b, &d); err != nil {
 			panic(err)
